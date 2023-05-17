8e0da1ceb474a27c07216b460b9d5775991c95d4
Ev-genia <bzhenya2006@ya.ru>
2023-05-17 16:44:07 +0300

    fix multiplication matrixes

--- 02/00.cpp
+++ 02/00.cpp
@@ -11,32 +11,32 @@ int converter(std::string &str) {
 void enterCount(int &line, int &column) {
   std::string str;
 
-  std::cout << "Enter count of lines: " << std::endl;
-  std::cin >> line;
-  std::cout << "Enter count of colunms: " << std::endl;
-  std::cin >> column;
-  getline(std::cin, str);
+  // std::cout << "Enter count of lines: " << std::endl;
+  std::cin >> line >> column;
+  // std::cout << "Enter count of colunms: " << std::endl;
+  // getline(std::cin, str);
 }
 
 int **enterData(int &line, int &column) {
-  std::cout << "Enter elements: " << std::endl;
-std::cout << "&line: " << line << ", &column: " << column << std::endl;
+  // std::cout << "Enter elements: " << std::endl;
+// std::cout << "&line: " << line << ", &column: " << column << std::endl;
   int **rez = new int *[line];
   for (int i = 0; i < line; i++) {
     rez[i] = new int[column];
     bzero(rez[i], column);
     for (int j = 0; j < column; j++) {
-      std::string str;
-      getline(std::cin, str);
-      rez[i][j] = converter(str);
-std::cout << "rez[" << i << "][" << j << "]: " << rez[i][j] << std::endl;
+      // std::string str;
+      // getline(std::cin, str);
+      // rez[i][j] = converter(str);
+      std::cin >> rez[i][j];
+// std::cout << "rez[" << i << "][" << j << "]: " << rez[i][j] << std::endl;
     }
   }
   return (rez);
 }
 
 void printArr(int **arr, int line, int column) {
-std::cout << "Array: " << std::endl;
+// std::cout << "Array: " << std::endl;
 
   for (int i = 0; i < line; i++) {
     for (int j = 0; j < column; j++)
@@ -45,19 +45,14 @@ std::cout << "Array: " << std::endl;
   }
 }
 
-int sumElem(int **arr1, int **arr2, int j, int column2) {
-std::cout << "column2 = " << column2 << std::endl;
-std::cout << "j = " << j << std::endl;
-  if (column2 > 0) {
-    column2--;
-std::cout << "arr1[j][k] * arr2[k][j] = " << arr1[j][column2] * arr2[column2][j] << std::endl;
-std::cout << "arr1[" << j << "][" << column2 << "]: " << arr1[j][column2] << std::endl;
-std::cout << "arr2[" << column2 << "][" << j << "]: " << arr2[column2][j] << std::endl;
-    return (arr1[j][column2] * arr2[column2][j] + sumElem(arr1, arr2, j, column2));
+int sumElem(int *arr1, int **arr2, int j, int column2) {
+  int rez = 0;
+
+  for (int i = 0; i < column2; i++) {
+    rez += arr1[i] * arr2[i][j];
   }
-  else {
-    return (0);
-  }
+// std::cout << "rez: " << rez <<std::endl;
+  return (rez);
 }
 
 int **handler(int **arr1, int line1, int column1, int **arr2, int line2, int column2) {
@@ -65,8 +60,8 @@ int **handler(int **arr1, int line1, int column1, int **arr2, int line2, int col
   for (int i = 0; i < line1; i++) {
     rez[i] = new int[column2];
     for (int j = 0; j < column2; j++) {
-      rez[i][j] = sumElem(arr1, arr2, j, column2);
-std::cout << "rez[" << i << "][" << j << "]: " << rez[i][j] << std::endl;
+      rez[i][j] = sumElem(arr1[i], arr2, j, column2);
+// std::cout << "rez[" << i << "][" << j << "]: " << rez[i][j] << std::endl;
     }
   }
   return (rez);
@@ -92,9 +87,9 @@ int main() {
     return (1);
   }
   arr1 = enterData(line1, column1);
-printArr(arr1, line1, column1);
+// printArr(arr1, line1, column1);
   arr2 = enterData(line2, column2);
-printArr(arr2, line2, column2);
+// printArr(arr2, line2, column2);
   arr3 = handler(arr1, line1, column1, arr2, line2, column2);
 printArr(arr3, line1, column2);
   freeArr(arr1, line1);
@@ -103,8 +98,9 @@ printArr(arr3, line1, column2);
   return (0);
 }
 
-// 2
-// 3
+// 2 3
+// 3 3
+
 // 1 2 3
 // 1 0 -1
 
@@ -115,3 +111,4 @@ printArr(arr3, line1, column2);
 // rez:
 // 36 7 25
 // -4 3 -3
+
